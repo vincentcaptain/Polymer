2e9293cfbba53a4d407bf8e7184ba7feb323fedd
vincentcaptain <push.default>
2017-03-03 11:50:02 -0800

    script

---
+++ time-dependent.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=time_dependent_protocol.py
+#
+# Account:
+#SBATCH --account=co_noneq
+#
+# Partition:
+#SBATCH --partition=savio2
+#
+# Request one node:
+#SBATCH --nodes=1
+#
+# Processors per task:
+#SBATCH --cpus-per-task=20
+#
+# Wall clock limit:
+#SBATCH --time=250:00:00
+#
+# Mail type:
+#SBATCH --mail-type=all
+#
+# Mail user:
+#SBATCH --mail-user=vincentcaptain@berkeley.edu
+## Command(s) to run:
+python2.7 time_dependent_protocol.py
