fa658ae94344f9e4802677bb63907749571b0f4f
vincentcaptain <push.default>
2017-03-18 22:42:18 -0700

    runs remaining

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 from __future__ import division
 from lammps import PyLammps
 from lammps import lammps
+import sys
 p = lammps()
 p.file("in.ljwall")
-i = 0
-while i < 16:
-	j = i / 2
-	p.command("fix 3 polymer spring tether 5 %d NULL NULL 0" % j)
+i = 46
+while i < 60:
+	p.command("fix 3 polymer spring tether 5 %d NULL NULL 0" % i)
 	p.command("run 500")
 	p.command("unfix 3")
-	i += 1
+	i += 2
+sys.exit()
