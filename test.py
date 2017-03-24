51b2ccb5fd396a64060446268fefaa0015daf201
Shujia Liang <vincentcaptain@berkeley.edu>
2017-03-24 11:18:09 -0700

    MPI

--- test.py
+++ test.py
@@ -2,10 +2,13 @@ from __future__ import division
 from lammps import PyLammps
 from lammps import lammps
 import sys
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+print "Proc %d out of %d procs" % (comm.Get_rank(),comm.Get_size())
 p = lammps()
 p.file("in.ljwall")
 i = 46
-while i < 60:
+while i < 50:
 	p.command("fix 3 polymer spring tether 5 %d NULL NULL 0" % i)
 	p.command("run 500")
 	p.command("unfix 3")
