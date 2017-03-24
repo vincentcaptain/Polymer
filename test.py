58dfeaf8d357c30ad2e9e00e16a4e2616ffaee0f
Shujia Liang <vincentcaptain@berkeley.edu>
2017-03-24 11:43:59 -0700

    new

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ print "Proc %d out of %d procs" % (comm.Get_rank(),comm.Get_size())
 p = lammps()
 p.file("in.ljwall")
 i = 0
-while i < 10:
+while i < 2:
 	p.command("fix 3 polymer spring tether 5 %d NULL NULL 0" % i)
-	p.command("run 5000")
+	p.command("run 1000000")
 	p.command("unfix 3")
 	i += 2
 sys.exit()
