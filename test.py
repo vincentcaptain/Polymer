ce37c34d86dc102c1adfe0859a4ccdaf084bc700
vincentcaptain <liangshuja@gmail.com>
2017-03-27 10:40:20 -0700

    all

--- test.py
+++ test.py
@@ -7,18 +7,18 @@ comm = MPI.COMM_WORLD
 print "Proc %d out of %d procs" % (comm.Get_rank(),comm.Get_size())
 p = lammps()
 p.file("in.ljwall")
-#i = 0
-#while i < 24:
-#	p.command("fix 3 polymer spring tether 3 %d NULL NULL 0" % i)
-#	p.command("run 500000")
-#	p.command("unfix 3")
-#	i += 1
-i = 35
-while i < 60:
+i = 0
+while i < 24:
 	p.command("fix 3 polymer spring tether 3 %d NULL NULL 0" % i)
-        p.command("run 500000")
-        p.command("unfix 3")
-        i += 1
+	p.command("run 500000")
+	p.command("unfix 3")
+	i += 1
+# i = 35
+# while i < 60:
+# 	p.command("fix 3 polymer spring tether 3 %d NULL NULL 0" % i)
+#         p.command("run 500000")
+#         p.command("unfix 3")
+#         i += 1
 # i = 24
 # while i < 35:
 #         p.command("fix 3 polymer spring tether 1 %d NULL NULL 0" % i)
