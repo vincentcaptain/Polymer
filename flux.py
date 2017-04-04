231bc32c3478dbaf70d8fd4435200fe0b4e2d443
vincentcaptain <push.default>
2017-04-04 14:52:10 -0700

    new

---
+++ flux.py
@@ -0,0 +1,12 @@
+from __future__ import division
+from lammps import lammps
+import sys
+from mpi4py import MPI
+p = lammps()
+p.file("in.ljwall")
+i = 0
+p.command("fix 3 polymer spring tether 1 30 NULL NULL 0")
+n = p.extract_compute("com", 0, 1)[0]
+j = 0
+p.command("run 15000000")
+sys.exit()
